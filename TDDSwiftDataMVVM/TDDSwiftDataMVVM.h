ec800d2960ed57339673a57cdbdd66b875340e34
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-12-08 21:31:10 +0530

    Initial Commit

---
+++ TDDSwiftDataMVVM/TDDSwiftDataMVVM.h
@@ -0,0 +1,18 @@
+//
+//  TDDSwiftDataMVVM.h
+//  TDDSwiftDataMVVM
+//
+//  Created by Mushthak Ebrahim on 08/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDDSwiftDataMVVM.
+FOUNDATION_EXPORT double TDDSwiftDataMVVMVersionNumber;
+
+//! Project version string for TDDSwiftDataMVVM.
+FOUNDATION_EXPORT const unsigned char TDDSwiftDataMVVMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDSwiftDataMVVM/PublicHeader.h>
+
+
