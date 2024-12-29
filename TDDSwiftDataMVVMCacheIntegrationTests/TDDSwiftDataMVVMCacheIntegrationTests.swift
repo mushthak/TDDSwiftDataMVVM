6e4c7c7f6aa1dafb3607f3b2b7f8d480b9ce15d6
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-12-29 17:11:36 +0530

    `LocaleUserLoader` in integration delivers no items on empty cache

--- TDDSwiftDataMVVMCacheIntegrationTests/TDDSwiftDataMVVMCacheIntegrationTests.swift
+++ TDDSwiftDataMVVMCacheIntegrationTests/TDDSwiftDataMVVMCacheIntegrationTests.swift
@@ -6,5 +6,31 @@
 //
 
 import Testing
+import SwiftData
+import TDDSwiftDataMVVM
 
-struct TDDSwiftDataMVVMCacheIntegrationTests {}
+struct TDDSwiftDataMVVMCacheIntegrationTests {
+
+    @Test func test_load_deliversNoItemsOnEmptyCache() async {
+        do {
+            let sut = makeSUT()
+            let result: [User] = try await sut.loadUsers()
+            #expect(result.isEmpty)
+        } catch  {
+            Issue.record("Expected success but got \(error) intead")
+        }
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocaleUserLoader {
+        let schema = Schema([
+            ManagedUser.self,
+        ])
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: schema, configurations: [config])
+        let store = SwiftDataStore(modelContainer: container)
+        let sut = LocaleUserLoader(store: store)
+        return sut
+    }
+
+}
