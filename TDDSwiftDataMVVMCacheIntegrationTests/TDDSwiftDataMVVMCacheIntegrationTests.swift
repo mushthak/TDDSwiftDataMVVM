08012781f937706bf090887bd530c0c75fc9c868
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2024-12-29 17:10:32 +0530

    Add new unit target for cache integration tests

---
+++ TDDSwiftDataMVVMCacheIntegrationTests/TDDSwiftDataMVVMCacheIntegrationTests.swift
@@ -0,0 +1,10 @@
+//
+//  TDDSwiftDataMVVMCacheIntegrationTests.swift
+//  TDDSwiftDataMVVMCacheIntegrationTests
+//
+//  Created by Mushthak Ebrahim on 29/12/24.
+//
+
+import Testing
+
+struct TDDSwiftDataMVVMCacheIntegrationTests {}
