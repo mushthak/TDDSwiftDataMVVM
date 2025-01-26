05051eecbb0f211c22c75db39c665cb6cc9c5f6d
Mushthak Ebrahim <mushthakebrahim@gmail.com>
2025-01-26 12:05:26 +0530

    Create workspace for App composition

---
+++ TDDSwiftDataMVVMApp/TDDSwiftDataMVVMApp/TDDSwiftDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDSwiftDataMVVMAppApp.swift
+//  TDDSwiftDataMVVMApp
+//
+//  Created by Mushthak Ebrahim on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TDDSwiftDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDDSwiftDataMVVMApp/TDDSwiftDataMVVMAppTests/TDDSwiftDataMVVMAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TDDSwiftDataMVVMAppTests.swift
+//  TDDSwiftDataMVVMAppTests
+//
+//  Created by Mushthak Ebrahim on 26/01/25.
+//
+
+import Testing
+@testable import TDDSwiftDataMVVMApp
+
+struct TDDSwiftDataMVVMAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
